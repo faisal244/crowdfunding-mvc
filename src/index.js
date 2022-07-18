02237095a4e8a8fa70a5666453677b3bef881b1b
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-07-18 03:42:58 +0100

    Completed session and cookie setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
+const connectSessionSequelize = require("connect-session-sequelize");
 
 const routes = require("./routes");
 const connection = require("./config/connection");
+
+const SequelizeStore = connectSessionSequelize(session.Store);
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+const sessionOptions = {
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false,
+	cookies: {
+		maxAge: 3600 * 1000,
+		httpOnly: true,
+		secure: false,
+		sameSite: "strict",
+	},
+	store: new SequelizeStore({
+		db: connection,
+	}),
+};
+
+app.use(session(sessionOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
